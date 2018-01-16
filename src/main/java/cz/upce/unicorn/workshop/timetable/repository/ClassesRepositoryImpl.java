a7dadb6bc310a1fee2c1fca284fe6312b3d61caf
petrfilip <mujfibi@gmail.com>
2018-01-16 06:47:17 +0100

    Reimplementation - without spring boot jpa

--- src/main/java/cz/upce/unicorn/workshop/timetable/repository/ClassesRepositoryImpl.java
+++ src/main/java/cz/upce/unicorn/workshop/timetable/repository/ClassesRepositoryImpl.java
@@ -1,20 +1,51 @@
 package cz.upce.unicorn.workshop.timetable.repository;
 
 import cz.upce.unicorn.workshop.timetable.entity.Classes;
+import cz.upce.unicorn.workshop.timetable.entity.Course;
 import cz.upce.unicorn.workshop.timetable.model.TimeEnum;
+import cz.upce.unicorn.workshop.timetable.service.ServiceLocator;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.*;
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClassesRepositoryImpl extends AbstractRepositoryImpl<Classes> implements ClassesRepository {
     @Override
     public List<Classes> findByCourseId(Integer id) {
-        return null;
+        EntityManager entityManager = ServiceLocator.createEntityManager();
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Classes> cq = cb.createQuery(getClazz());
+
+        Root<Classes> root = cq.from(getClazz());
+        Join<Classes, Course> join = root.join("course");
+
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        predicates.add(cb.equal(join.get("id"), id));
+
+        cq.select(root).where(predicates.toArray(new Predicate[]{}));
+        return entityManager.createQuery(cq).getResultList();
     }
 
     @Override
-    public List<Classes> findByDayOfWeekAndTimeAndRoom(DayOfWeek dayOfWeek, TimeEnum timeEnum, String room) {
-        return null;
+    public List<Classes> findByDayOfWeekAndTimeAndRoom(DayOfWeek dayOfWeek, TimeEnum timeEnum, String room) { //todo not tested yes
+        EntityManager entityManager = ServiceLocator.createEntityManager();
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Classes> cq = cb.createQuery(getClazz());
+
+        Root<Classes> root = cq.from(getClazz());
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+
+        predicates.add(cb.equal(root.get("dayOfWeek"), dayOfWeek));
+        predicates.add(cb.equal(root.get("time"), timeEnum));
+        predicates.add(cb.equal(root.get("room"), room));
+
+        cq.select(root).where(predicates.toArray(new Predicate[]{}));
+
+        return entityManager.createQuery(cq).getResultList();
     }
 
     @Override
--- src/main/java/cz/upce/unicorn/workshop/timetable/repository/EnrollmentRepositoryImpl.java
+++ src/main/java/cz/upce/unicorn/workshop/timetable/repository/EnrollmentRepositoryImpl.java
@@ -1,23 +1,64 @@
 package cz.upce.unicorn.workshop.timetable.repository;
 
+import cz.upce.unicorn.workshop.timetable.entity.Classes;
 import cz.upce.unicorn.workshop.timetable.entity.Enrollment;
+import cz.upce.unicorn.workshop.timetable.entity.Student;
+import cz.upce.unicorn.workshop.timetable.service.ServiceLocator;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EnrollmentRepositoryImpl extends AbstractRepositoryImpl<Enrollment> implements EnrollmentRepository {
     @Override
     public List<Enrollment> findEnrollmentByClassesId(Integer id) {
-        return null;
+        EntityManager entityManager = ServiceLocator.createEntityManager();
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Enrollment> cq = cb.createQuery(getClazz());
+
+        Root<Enrollment> root = cq.from(getClazz());
+        Join<Enrollment, Classes> join = root.join("classes");
+
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        predicates.add(cb.equal(join.get("id"), id));
+
+        cq.select(root).where(predicates.toArray(new Predicate[]{}));
+        return entityManager.createQuery(cq).getResultList();
     }
 
     @Override
     public List<Enrollment> findEnrollmentByStudentId(Integer id) {
-        return null;
+        EntityManager entityManager = ServiceLocator.createEntityManager();
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Enrollment> cq = cb.createQuery(getClazz());
+
+        Root<Enrollment> root = cq.from(getClazz());
+        Join<Enrollment, Student> join = root.join("student");
+
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        predicates.add(cb.equal(join.get("id"), id));
+
+        cq.select(root).where(predicates.toArray(new Predicate[]{}));
+        return entityManager.createQuery(cq).getResultList();
     }
 
     @Override
     public List<Enrollment> findEnrollmentsByClassesIdAndStudentId(Integer classesId, Integer studentId) {
-        return null;
+        EntityManager entityManager = ServiceLocator.createEntityManager();
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Enrollment> cq = cb.createQuery(getClazz());
+
+        Root<Enrollment> root = cq.from(getClazz());
+        Join<Enrollment, Student> joinS = root.join("student");
+        Join<Enrollment, Classes> joinC = root.join("classes");
+
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        predicates.add(cb.equal(joinS.get("id"), studentId));
+        predicates.add(cb.equal(joinC.get("id"), classesId));
+
+        cq.select(root).where(predicates.toArray(new Predicate[]{}));
+        return entityManager.createQuery(cq).getResultList();
     }
 
     @Override
